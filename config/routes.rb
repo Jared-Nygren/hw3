5a2bc6937f375e540687d4166e480a80947671d1
Jared Nygren <jared.nygren@kellogg.northwestern.edu>
2024-02-22 00:27:09 +0000

    Comments on routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 
+   # ***FYI Had some routing issues and ended up deviating from class code structure while troubleshooting with GPT assistance -- 
+
    # Define the root path route ("/")
    root "places#index"
 
-   # ***Deviated from class structure here with GPT assistance -- 
    resources :places do
      # Nested routes for Entries within Places, for creating new entries
      resources :entries, only: [:new, :create]
